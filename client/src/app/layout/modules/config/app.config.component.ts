fd050bf62274f10bde2d70e7de6690ba715004ab
s2000promax <s2000promax@gmail.com>
2023-08-04 15:08:28 +0400

    feat: refactor app config component

--- client/src/app/layout/modules/config/app.config.component.ts
+++ client/src/app/layout/modules/config/app.config.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LayoutService } from '@services/app.layout.service';
 import { ThemeStyles } from '@config/styles/themesStyles';
 import { ColorStyles } from '@config/styles/colorStyles';
@@ -9,15 +9,85 @@ import { TranslateService } from '@ngx-translate/core';
     selector: 'app-config',
     templateUrl: './app.config.component.html',
 })
-export class AppConfigComponent {
+export class AppConfigComponent implements OnInit {
     protected readonly ThemeStyles = ThemeStyles;
     protected readonly ColorsStyles = ColorStyles;
     protected readonly Languages = Languages;
+
+    languageItems!: Array<{
+        src: string;
+        language: Languages;
+    }>;
+
+    themesItems!: Array<{
+        src: string;
+        themeStyle: ThemeStyles;
+        colorStyle: ColorStyles;
+    }>;
 
     constructor(
         public layoutService: LayoutService,
         public translate: TranslateService,
     ) {}
+
+    ngOnInit() {
+        this.initializeLanguagesItems();
+        this.initializeThemesItems();
+    }
+
+    private initializeLanguagesItems(): void {
+        this.languageItems = [
+            { language: Languages.ENG, src: 'usa.png' },
+            { language: Languages.BEL, src: 'belarus.png' },
+            { language: Languages.PL, src: 'poland.png' },
+            { language: Languages.GE, src: 'georgia.png' },
+        ];
+    }
+
+    private initializeThemesItems(): void {
+        this.themesItems = [
+            {
+                themeStyle: ThemeStyles.LIGHT_INDIGO,
+                colorStyle: ColorStyles.LIGHT,
+                src: 'lara-light-indigo.png',
+            },
+            {
+                themeStyle: ThemeStyles.LIGHT_BLUE,
+                colorStyle: ColorStyles.LIGHT,
+                src: 'lara-light-blue.png',
+            },
+            {
+                themeStyle: ThemeStyles.LIGHT_PURPLE,
+                colorStyle: ColorStyles.LIGHT,
+                src: 'lara-light-purple.png',
+            },
+            {
+                themeStyle: ThemeStyles.LIGHT_TEAL,
+                colorStyle: ColorStyles.LIGHT,
+                src: 'lara-light-teal.png',
+            },
+            {
+                themeStyle: ThemeStyles.DARK_INDIGO,
+                colorStyle: ColorStyles.DARK,
+                src: 'lara-dark-indigo.png',
+            },
+            {
+                themeStyle: ThemeStyles.DARK_BLUE,
+                colorStyle: ColorStyles.DARK,
+                src: 'lara-dark-blue.png',
+            },
+            {
+                themeStyle: ThemeStyles.DARK_PURPLE,
+                colorStyle: ColorStyles.DARK,
+                src: 'lara-dark-purple.png',
+            },
+            {
+                themeStyle: ThemeStyles.DARK_TEAL,
+                colorStyle: ColorStyles.DARK,
+                src: 'lara-dark-teal.png',
+            },
+        ];
+    }
 
     get visible(): boolean {
         return this.layoutService.state.configSidebarVisible;
