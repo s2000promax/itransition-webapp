f9121a8fbcaa8b23d41e3ab0caa20a0487a45d7a
s2000promax <s2000promax@gmail.com>
2023-08-07 18:01:09 +0400

    fix: for deploy

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,17 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 import { SwaggerModule } from '@nestjs/swagger';
 import swaggerConfig from './config/app/swaggerConfig';
 import appConfig from './config/app/appConfig';
-
-// import cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableShutdownHooks();
     app.enableCors();
-    // app.use(cookieParser());
+    app.use(cookieParser());
     app.setGlobalPrefix('api');
 
     const document = SwaggerModule.createDocument(app, swaggerConfig());
