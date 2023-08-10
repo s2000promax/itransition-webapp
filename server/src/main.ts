c6b734dbfd275096917d8040db9eb44930527295
s2000promax <s2000promax@gmail.com>
2023-08-10 04:23:14 +0400

    fix: headers, cors origin

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,8 +9,18 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableShutdownHooks();
     app.enableCors({
+        allowedHeaders: [
+            'content-type',
+            'Access-Control-Allow-Origin',
+            'Access-Control-Allow-Credentials',
+            'Authorization',
+            'Accept',
+        ],
         origin: 'https://itransition-4.netlify.app',
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
         credentials: true,
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
     });
     app.use(cookieParser());
     app.setGlobalPrefix('api');
@@ -24,4 +34,5 @@ async function bootstrap() {
 
     await app.listen(port);
 }
+
 bootstrap();
